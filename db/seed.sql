2206572e21e1afafbfc263bf32d63c44370dbd2f
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-05-17 14:32:40 +0800

    added temp.js to gitignore. Updated name of employee schema to employee-schema.sql. added comments in seed.sql. wrote function to run upon app initialisation.

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,5 @@
 -- This is a file to pre-populate your database. This will make development of individual features much easier.
+-- dummy data
+
+INSERT INTO asdas
+VALUES sdavs
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
 	// Your username
 	user: "root",
 	// Be sure to update with your own MySQL password!
-	password: "",
+	password: "yourRootPassword",
 	database: "employee_DB",
 });
 
@@ -21,21 +21,82 @@ connection.connect((err) => {
 
 // Inquirer Questions
 
-// Functions for search/view
-function runSearch();
+const operations = {
+    // Search Data
+    "Search For An Employee": employeeSearch,
+    "Search For Department": departmentSearch,
+    // View Data
+    "View All Employees": viewAllEmployees,
+    "View All Employees By Department": viewEmployeesByDepartment,
+    "View All Employees By Manager": viewEmployeesByManager,
+    "View Total Utilized Budget Of A Department": departmentBudgetView,
+     // Add Data
+    "Add Employee": addEmployee,
+    "Add Department": addDepartment,
+    "Add Role": addRole,
+    // Update Data
+    "Update Employee Role": updateEmployee,
+    "Update Employee Manager": updateEmployeeManager,
+    // Delete Data
+    "Remove Employee": removeEmployee,
+    "Remove Role": removeRole,
+    "Remove Department": removeDepartment
+}
 
+// First function upon app initialisation
+const runSearch = () => {
+    inquirer
+    .prompt({
+        name: "action",
+        type: "list",
+        message: "Welcome to the Employee Tracker application. What would you like to do today?",
+        choices: Object.keys(operations),
+        choices: [
+            "Search For An Employee",
+            "Search For Department",
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "View Total Utilized Budget Of A Department",
+            "Add Employee",
+            "Add Department",
+            "Add Role",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "Remove Employee",
+            "Remove Role",
+            "Remove Department"
+        ]
+    })
+    .then((answer) => {
+        operations[answer.action]();
+    })
+};
+
+// modularise functions below
+
+
+// search.js
 function employeeSearch();
-
-function budgetView();
-
-function viewEmployeesByManager();
-
-// Functions for adding
-function addEmployee();
 
 function departmentSearch();
 
+function viewAllEmployees();
+
+function viewEmployeesByDepartment();
+
+function viewEmployeesByManager();
+
+function departmentBudgetView();
+
+
+// add.js
+// Functions for adding
+function addEmployee();
+
 function addDepartment();
+
+function addRole();
 
 // Functions for updating
 function updateEmployee();
@@ -43,8 +104,8 @@ function updateEmployee();
 function updateEmployeeManager();
 
 // Function for deleting
-function deleteDepartment();
+function removeEmployee();
 
-function deleteRoles();
+function removeRole();
 
-function deleteEmployee();
+function removeDepartment();
