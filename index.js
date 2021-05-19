9d29eac34740d948e6bcdc68309594497e2fb0b5
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-05-19 17:35:24 +0800

    added functions for employee search (by firstname), view all employees, department search. Other functions still WIP. Testing required.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Dependencies
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const cTable = require("console.table");
+// Modularised functions
+// const { employeeSearch, departmentSearch } = require('./operations/search');
 
 const connection = mysql.createConnection({
 	host: "localhost",
@@ -15,97 +18,154 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
 	if (err) throw err;
-    // Function to initiate search/app
+	// Function to initiate search/app
 	runSearch();
 });
 
-// Inquirer Questions
-
 const operations = {
-    // Search Data
-    "Search For An Employee": employeeSearch,
-    "Search For Department": departmentSearch,
-    // View Data
-    "View All Employees": viewAllEmployees,
-    "View All Employees By Department": viewEmployeesByDepartment,
-    "View All Employees By Manager": viewEmployeesByManager,
-    "View Total Utilized Budget Of A Department": departmentBudgetView,
-     // Add Data
-    "Add Employee": addEmployee,
-    "Add Department": addDepartment,
-    "Add Role": addRole,
-    // Update Data
-    "Update Employee Role": updateEmployee,
-    "Update Employee Manager": updateEmployeeManager,
-    // Delete Data
-    "Remove Employee": removeEmployee,
-    "Remove Role": removeRole,
-    "Remove Department": removeDepartment
-}
+	// Search Data
+	"Search For An Employee": employeeSearch,
+	"Search For Department": departmentSearch,
+	// View Data
+	"View All Employees": viewAllEmployees,
+	"View All Employees By Department": viewEmployeesByDepartment,
+	"View All Employees By Manager": viewEmployeesByManager,
+	"View Total Utilized Budget Of A Department": departmentBudgetView,
+	// Add Data
+	"Add Employee": addEmployee,
+	"Add Department": addDepartment,
+	"Add Role": addRole,
+	// Update Data
+	"Update Employee Role": updateEmployee,
+	"Update Employee Manager": updateEmployeeManager,
+	// Delete Data
+	"Remove Employee": removeEmployee,
+	"Remove Role": removeRole,
+	"Remove Department": removeDepartment,
+	// Exit (TBD)
+	Exit: exitApp,
+};
 
 // First function upon app initialisation
 const runSearch = () => {
-    inquirer
-    .prompt({
-        name: "action",
-        type: "list",
-        message: "Welcome to the Employee Tracker application. What would you like to do today?",
-        choices: Object.keys(operations),
-        choices: [
-            "Search For An Employee",
-            "Search For Department",
-            "View All Employees",
-            "View All Employees By Department",
-            "View All Employees By Manager",
-            "View Total Utilized Budget Of A Department",
-            "Add Employee",
-            "Add Department",
-            "Add Role",
-            "Update Employee Role",
-            "Update Employee Manager",
-            "Remove Employee",
-            "Remove Role",
-            "Remove Department"
-        ]
-    })
-    .then((answer) => {
-        operations[answer.action]();
-    })
+	inquirer
+		.prompt({
+			name: "action",
+			type: "list",
+			message:
+				"Welcome to the Employee Tracker application. What would you like to do today?",
+			choices: Object.keys(operations),
+		})
+		.then((answer) => {
+			operations[answer.action]();
+		});
 };
 
 // modularise functions below
 
-
 // search.js
-function employeeSearch();
+const employeeSearch = () => {
+	inquirer
+		.prompt([
+			{
+				name: "employeeSearch",
+				type: "input",
+				message:
+					"Please enter the employee's first name to search for their details.",
+			},
+		])
+		.then((answer) => {
+			const query =
+				"SELECT employee.id, first_name, last_name, roles.title, department.name, employee.manager_id";
+			query +=
+				"FROM employee LEFT JOIN (roles LEFT JOIN department ON department.id = roles.department_id)";
+			query += "ON employee.role_id = roles.id WHERE employee.first_name = ?";
+			console.log(answer.employeeSearch);
+			connection.query(query, [answer.employeeSearch], (err, res) => {
+				if (err) throw err;
+				cTable(["ID", "FirstName", "LastName", "Department"], res);
+				runSearch();
+			});
+		});
+};
 
-function departmentSearch();
+const departmentSearch = () => {
+	inquirer
+		.prompt([
+			{
+				name: "department",
+				type: "input",
+				message: "Please enter a department name to search for its details.",
+			},
+		])
+		.then((answer) => {
+			const query = `SELECT employee.id, first_name, last_name, role_id, department.name, roles.title, roles.salary `;
+			query += `FROM employee LEFT JOIN (roles LEFT JOIN department ON department.id = roles.department_id) `;
+			query += `ON employee.role_id = roles.id WHERE department.name = ?`;
+			console.log(answer.department);
+			connection.query(query, [answer.department], (err, res) => {
+				if (err) throw err;
+				cTable(["ID", "FirstName", "LastName", "Department"], res);
+				runSearch();
+			});
+		});
+};
 
-function viewAllEmployees();
+const viewAllEmployees = () => {
+	const query =
+		"SELECT employee.id, first_name, last_name, roles.title, department.name, employee.manager_id";
+	query +=
+		"FROM employee LEFT JOIN (roles LEFT JOIN department ON department.id = roles.department_id)";
+	query += "ON employee.role_id = roles.id ORDER BY employee.id";
+	connection.query(query, (err, res) => {
+		if (err) throw err;
+		cTable(res);
+		runSearch();
+	});
+};
 
-function viewEmployeesByDepartment();
+const viewEmployeesByDepartment = () => {};
 
-function viewEmployeesByManager();
+const viewEmployeesByManager = () => {};
 
-function departmentBudgetView();
-
+const departmentBudgetView = () => {
+    inquirer
+    .prompt [(
+        {
+            name:"budget",
+            type:"input",
+            message:"Please enter a department name to see its total department budget utilization (per annum).",
+        }
+    )]
+	const query = "SELECT department.id, department.name, roles.title, roles.salary";
+    query += "department JOIN roles ON department.id = roles.department_id";
+    query += "WHERE department.name = ?";
+	connection.query(query, function (err, res) {
+		if (err) throw err;
+		cTable(res);
+		runSearch();
+	});
+};
 
 // add.js
 // Functions for adding
-function addEmployee();
+const addEmployee = () => {};
 
-function addDepartment();
+const addDepartment = () => {};
 
-function addRole();
+const addRole = () => {};
 
 // Functions for updating
-function updateEmployee();
+const updateEmployee = () => {};
 
-function updateEmployeeManager();
+const updateEmployeeManager = () => {};
 
 // Function for deleting
-function removeEmployee();
+const removeEmployee = () => {};
 
-function removeRole();
+const removeRole = () => {};
 
-function removeDepartment();
+const removeDepartment = () => {};
+
+// Function for exiting app
+const exitApp = () => {};
